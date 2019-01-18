31eefec0f2c5942f29466bd1453e3b2e48f109f3
alex.paclin <alex.paclin@gmail.com>
2019-01-18 15:57:19 +0500

    yield

--- Queries/Program.cs
+++ Queries/Program.cs
@@ -17,9 +17,16 @@ namespace Queries
                 new Movie() { Title = "Start Wars V", Rating = 8.7f, Year = 1980 }
             };
 
+            //var query = movies.Where(m => m.Year > 2000);
+
+            //foreach (var movie in query)
+            //{
+            //    Console.WriteLine(movie.Title);
+            //}
+
             var query = movies.Filter(m => m.Year > 2000);
 
-            foreach(var movie in query)
+            foreach (var movie in query)
             {
                 Console.WriteLine(movie.Title);
             }
